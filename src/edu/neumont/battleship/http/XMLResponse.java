531002e180524f64566d2ae725863bd46831fa24
Joseph Bass <jab416171@gmail.com>
2011-07-28 11:06:41 -0600

    aded xmlresponse class

---
+++ src/edu/neumont/battleship/http/XMLResponse.java
@@ -0,0 +1,10 @@
+package edu.neumont.battleship.http;
+
+public class XMLResponse {
+	private String rawXML;
+	
+	public XMLResponse(String rawXML) {
+		// TODO make this object useful
+		this.rawXML = rawMXL;
+	}
+}
