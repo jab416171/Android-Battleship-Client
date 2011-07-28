48a082d27d97edbb0231494ace38ef176195a9b3
Joseph Bass <jab416171@gmail.com>
2011-07-28 13:43:29 -0600

    updated xml response

--- src/edu/neumont/battleship/http/XMLResponse.java
+++ src/edu/neumont/battleship/http/XMLResponse.java
@@ -2,9 +2,21 @@ package edu.neumont.battleship.http;
 
 public class XMLResponse {
 	private String rawXML;
+	private int gameID;
+	private String result;
+	private String winner;
+	
+	
+	// Do we want to make a game object?
+	// What's the best way to represent the different types of responses?
 	
 	public XMLResponse(String rawXML) {
 		// TODO make this object useful
 		this.rawXML = rawXML;
 	}
+}
+// incomplete...
+enum ResponseType {
+	error, simpleResult, 
+	
 }
