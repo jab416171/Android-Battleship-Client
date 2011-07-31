a40fd0a45bb8e1d92a2c03713d3442a70ffa663f
Joseph Bass <jab416171@gmail.com>
2011-07-31 10:46:51 -0600

    added private

--- src/edu/neumont/battleship/model/FireStatus.java
+++ src/edu/neumont/battleship/model/FireStatus.java
@@ -3,9 +3,10 @@ package edu.neumont.battleship.model;
 public enum FireStatus {
 	HIT, MISS, SUNK, UNKNOWN;
 
-	FireStatus(){}
-	public static FireStatus fromXml(String xml)
-	{
+	private FireStatus() {
+	}
+
+	public static FireStatus fromXml(String xml) {
 		return Enum.valueOf(FireStatus.class, xml);
 	}
 }
