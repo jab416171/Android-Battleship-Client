dd4c0d8a15782555fd873fdafff2a0ff0efff1b0
Joseph Bass <jab416171@gmail.com>
2011-07-31 11:02:52 -0600

    fixed cookies model

---
+++ src/edu/neumont/battleship/model/Cookie.java
@@ -0,0 +1,18 @@
+package edu.neumont.battleship.model;
+
+public class Cookie {
+	private String jSessionId;
+
+	public Cookie() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getjSessionId() {
+		return jSessionId;
+	}
+
+	public void setjSessionId(String jSessionId) {
+		this.jSessionId = jSessionId;
+	}
+
+}
