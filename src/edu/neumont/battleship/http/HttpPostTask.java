5d9a1a9074e0238ef364f4010251855fee3c310d
Joseph Bass <jab416171@gmail.com>
2011-08-15 11:41:26 -0600

    added HttpPostTask, still unsure where we can use it.

---
+++ src/edu/neumont/battleship/http/HttpPostTask.java
@@ -0,0 +1,55 @@
+package edu.neumont.battleship.http;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.AsyncTask;
+import edu.neumont.battleship.R;
+
+public class HttpPostTask extends AsyncTask<String, Void, String>
+{
+	ProgressDialog pd;
+	Context ctx;
+	
+	public HttpPostTask(Context ctx, String title)
+	{
+		this.ctx = ctx;
+		pd = new ProgressDialog(ctx);
+		pd.setTitle(title);
+	}
+	
+	@Override
+	protected String doInBackground(String... params)
+	{
+		this.publishProgress();
+		try
+		{
+			return HttpHandler.postData(params[0], params[1]);
+		} catch (Exception e)
+		{
+			this.cancel(true);
+		}
+		return null;
+	}
+	
+	@Override
+	protected void onPostExecute(String result)
+	{
+		pd.dismiss();
+	}
+	
+	@Override
+	protected void onCancelled()
+	{
+		pd.dismiss();
+		new AlertDialog.Builder(ctx).setTitle(R.string.servernotfound)
+				.setPositiveButton("OK", new OnClickListener() {
+					public void onClick(DialogInterface dialog, int which)
+					{
+						dialog.dismiss();
+					}
+				}).show();
+	}
+}
