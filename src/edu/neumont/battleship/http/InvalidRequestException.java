1c09babe03885495bc2222cf667c8e4ea5fc8927
Joseph Bass <jab416171@gmail.com>
2011-08-28 17:36:41 -0600

    added Version IDs to the exceptions

--- src/edu/neumont/battleship/http/InvalidRequestException.java
+++ src/edu/neumont/battleship/http/InvalidRequestException.java
@@ -2,6 +2,11 @@ package edu.neumont.battleship.http;
 
 public class InvalidRequestException extends BattleshipException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public InvalidRequestException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
--- src/edu/neumont/battleship/http/InvalidXMLException.java
+++ src/edu/neumont/battleship/http/InvalidXMLException.java
@@ -2,6 +2,11 @@ package edu.neumont.battleship.http;
 
 public class InvalidXMLException extends BattleshipException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public InvalidXMLException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
