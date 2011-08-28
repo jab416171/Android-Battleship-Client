7f6ca79c70bed7458c55f1a4b6b0083eaee60a79
Glen Watson <gwatson@student.neumont.edu>
2011-08-28 15:11:46 -0600

    designed the responses in GameLogic, no implementation

--- src/edu/neumont/battleship/testharness/GameLogic.java
+++ src/edu/neumont/battleship/testharness/GameLogic.java
@@ -14,16 +14,31 @@ import edu.neumont.battleship.model.ShipType;
  */
 public interface GameLogic
 {
+	//no cookie, except for newGame
+	//bad gameId
+	//game is over
+	
 	public abstract int newGame(String playerName, PlayerType robot) throws IOException;
 	
 	public abstract int[] getGameList() throws IOException;
 	
+	// successful
+	// unsuccessful
+	//  off board
+	//  overlaps
+	//  already placed that ship
 	public abstract String placeShip(int gameId, String playerName, Coordinate coords, Direction direction, ShipType ship) throws Exception;
 	
+	//miss
+	//hit
+	//not your turn
 	public abstract String fire(int gameId, String playerName, Coordinate coords) throws IOException;
 	
+	// TODO: complicated
 	public abstract String update(int gameId, String playerName) throws IOException;
 	
+	//successful
+	//unsuccessful
 	public abstract String join(int gameId, String playerName) throws IOException;
 	
 	public abstract String forfeit(int gameId, String playerName) throws IOException;
